5ab774ff5d1a72c5e85f81dbbed8d9c069145697
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-06-02 15:31:35 +0900

    feat: add todo

--- client.c
+++ client.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+// todo
+// - ./client <host> <port>
+// - サーバーに接続する
+// - quit で退出する
+// - メッセージを送る
+// - メッセージを受け取る
+// - 名前を入力できるようにする
+
 int main() {
   return 0;
 }
--- server.c
+++ server.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+// todo
+// - ./server <port>
+// - ポートで待ち受ける
+// - スレッドを作る
+// - quit で終了する
+// - 複数クライアントに対応する
+// - ユーザー名の管理
+
 int main() {
   return 0;
 }
