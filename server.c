e45282afca72a4d1f88487913d9c2b660c36d453
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-06-03 18:23:38 +0900

    feat(server): connectまで

--- server.c
+++ server.c
@@ -1,4 +1,5 @@
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,7 +15,15 @@
 // - 複数クライアントに対応する
 // - ユーザー名の管理
 
-int main(int argc, char* argv[]) {
+void *handle_send(void *arg);
+void *handle_receive(void *arg);
+
+typedef struct {
+  struct sockaddr_in addr;
+  int sock;
+} client_t;
+
+int main(int argc, char *argv[]) {
   // Input validation
   if (argc != 2) {
     printf("%sinfo%s usage: %s <port>\n", COLOR_CYAN, COLOR_RESET, argv[0]);
@@ -41,18 +50,33 @@ int main(int argc, char* argv[]) {
 
   // Server Socket Address
   struct sockaddr_in server;
-  memset((void*) &server, 0, sizeof(server));
+  memset((void *) &server, 0, sizeof(server));
   server.sin_family = PF_INET;
   server.sin_port = htons(port);
   server.sin_addr.s_addr = htonl(INADDR_ANY);
 
   // Bind
-  if (bind(listening_socket, (struct sockaddr*) &server, sizeof(server)) == -1) {
+  if (bind(listening_socket, (struct sockaddr *) &server, sizeof(server)) == -1) {
     printf("%serror%s bind failed", COLOR_RED, COLOR_RESET);
     return 1;
   }
 
   // Listen
+  if (listen(listening_socket, 5) == -1) {
+    printf("%serror%s listen failed", COLOR_RED, COLOR_RESET);
+    return 1;
+  }
+  client_t client;
+  memset((void *) &client, 0, sizeof(client));
+  int connected_socket = accept(listening_socket, (struct sockaddr *) &client.addr, &(socklen_t) { sizeof(client.addr) });
+  if (connected_socket == -1) {
+    printf("%serror%s accept failed", COLOR_RED, COLOR_RESET);
+    return 1;
+  }
+
+  printf("%sinfo%s connected from %s:%d\n", COLOR_CYAN, COLOR_RESET, inet_ntoa(client.addr.sin_addr), ntohs(client.addr.sin_port));
+  close(listening_socket);
+
   while (true) {
   }
   return 0;
